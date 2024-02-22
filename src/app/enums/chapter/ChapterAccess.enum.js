071334a2b0325cd2a279606454e10ba825bbedcb
alan.nns <alan.nns.14.11@gmail.com>
2024-02-22 08:03:38 +0700

    feat: add chapter access enum

---
+++ src/app/enums/chapter/ChapterAccess.enum.js
@@ -0,0 +1,17 @@
+const ChapterAccessEnum = {
+  PRIVATE: 0,
+  PUBLIC: 1,
+
+  allName() {
+    return {
+      [this.PRIVATE]: 'chỉ mình tôi',
+      [this.PUBLIC]: 'mọi người',
+    }
+  },
+
+  getNameByValue(value) {
+    return this.allName()[value]
+  },
+}
+
+export default ChapterAccessEnum
