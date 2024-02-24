74e71b98234faec21d68ca22715f23d5f090675c
alan.nns <alan.nns.14.11@gmail.com>
2024-02-24 15:24:06 +0700

    update: update time clear folder uploads task

--- src/cron/ClearUploadFolder.task.js
+++ src/cron/ClearUploadFolder.task.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import fs from 'fs'
 import path from 'path'
 
-const ClearUploadFolderTask = cron.schedule('0 */1 * * *', async () => {
+const ClearUploadFolderTask = cron.schedule('*/30 * * * *', async () => {
   try {
     const uploadsFolder = path.join(__dirname, '..', 'uploads')
     const uploadFiles = fs.readdirSync(uploadsFolder)
@@ -12,7 +12,7 @@ const ClearUploadFolderTask = cron.schedule('0 */1 * * *', async () => {
       const timeUploadFile = uploadFile.split(/-|\./)[1]
       const timeDif = (Date.now() - timeUploadFile) / (1000 * 60)
 
-      if (timeDif > 30) {
+      if (timeDif > 20) {
         fileRemove.push(uploadFile)
         fs.rm(path.join(uploadsFolder, uploadFile), (err) => {
           console.log(err)
