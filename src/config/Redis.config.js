b1926655c0aeafd570e238993a72e45abd6ee06b
alan.nns <alan.nns.14.11@gmail.com>
2024-02-26 06:36:36 +0700

    update: update config to RedisConfig

--- src/config/Redis.config.js
+++ src/config/Redis.config.js
@@ -1,6 +1,16 @@
 import { createClient } from 'redis'
+import { configDotenv } from 'dotenv'
 
-const client = createClient()
+configDotenv()
+
+const client = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+})
   .on('error', (err) => console.log('Redis Client Error', err))
   .connect()
 
