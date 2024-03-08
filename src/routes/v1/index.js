14be65c629089e0fd4c325d4740156d5c35bffbe
alan.nns <alan.nns.14.11@gmail.com>
2024-03-08 16:23:47 +0700

    feat: add ratelimit middleware to block ddos

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,8 +3,12 @@ import AuthPackageRouter from './auth'
 import StoryPackageRouter from './story'
 import UtilPackageRouter from './util'
 import AuthMiddleware from '@/app/www/middleware/Auth.middleware'
+import RateLimitMiddleware from '@/app/www/middleware/RateLimit.middleware'
 // lấy ra bộ định tuyến
 const RouterV1 = express.Router()
+
+// giới hạn yêu cầu để chặn ddos
+RouterV1.use(RateLimitMiddleware.limitRequest())
 
 RouterV1.use(AuthPackageRouter)
 
