d759bd5fad921bc09cf9b292bfba3e4d89186f74
alan.nns <alan.nns.14.11@gmail.com>
2024-05-10 15:23:56 +0700

    update: add ping server task to fix kill process on render

--- src/cron/index.js
+++ src/cron/index.js
@@ -1,7 +1,9 @@
 import ClearUploadFolderTask from './ClearUploadFolder.task'
+import PingServerTask from './PingServer.task'
 import PublicChapterTask from './PublicChapter.task'
 
 const initCron = () => {
+  PingServerTask.start()
   ClearUploadFolderTask.start()
   PublicChapterTask.start()
 }
